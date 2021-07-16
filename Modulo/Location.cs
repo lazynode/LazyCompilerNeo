98a5aab3eb219598ff75e1798c2ab87fb292fe6f
vang1ong7ang <vang1ong7ang@outlook.com>
2021-07-16 11:43:54 +0000

    dev

--- Modulo/Location.cs
+++ Modulo/Location.cs
@@ -12,7 +12,7 @@ namespace LazyCompilerNeo
     {
         public class Location : Modulo
         {
-            public static readonly XNamespace NameSpace = nameof(Location);
+            public static readonly XNamespace ns = nameof(Location);
             static Dictionary<XElement, int> length = new();
             public Location(XElement node) : base(node)
             {
